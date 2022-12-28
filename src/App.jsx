fda4414cf122b86496710673f6f4168ee7b2b82a
fakeveldmort <dxgamergr@gmail.com>
2022-12-28 22:39:18 +0100

    fist commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import styles from "./style";
+
+import {
+  NavBar,
+  Billing,
+  CardDeal,
+  Business,
+  Clinets,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+} from "./componenets";
+
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <NavBar />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clinets />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
